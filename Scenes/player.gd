470e7440eea3f2a2aa5b06fbd02865ea56df4ac4
Gerard Patrick <gmbriones@studnet.apc.edu.ph>
2024-07-21 16:45:08 +0800

    update

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-
 var SPEED = 200.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -900.0
+var jump_damping = 35.0 # The rate at which the jump velocity is reduced
+
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	if (velocity.x > 1 || velocity.y < -1):
+	if (velocity.x > 1 or velocity.y < -1):
 		sprite_2d.animation = "running"
 	else:
 		sprite_2d.animation = "default"
@@ -20,15 +20,19 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# Smoothly stop the jump velocity when the jump button is released.
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = lerp(velocity.y, 0.0, jump_damping * delta)
+	
 	if Input.is_action_pressed("sprint"):
-		SPEED = 300.0
+		SPEED = 400.0
 	else:
 		SPEED = 200.0
-	
-	
+		
+	print(SPEED)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
@@ -37,11 +41,10 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 15)
-		
-
-	move_and_slide()
 	
-	if velocity.x != 0:
-		var isLeft = velocity.x < 0
-		sprite_2d.flip_h = isLeft
-		
+	# Move and slide with the current velocity
+	move_and_slide()  # No arguments needed here
+
+	# Determine sprite orientation based on velocity
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
