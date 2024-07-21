cd570dd0eff7abc9e36e51feb3b383497e9daef6
juriyuzu <98145551+juriyuzu@users.noreply.github.com>
2024-07-21 16:02:31 +0800

    Update

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 var SPEED = 200.0
-const JUMP_VELOCITY = -900.0
+const JUMP_VELOCITY = -700.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -24,11 +24,11 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_pressed("sprint"):
-		SPEED = 400.0
+		SPEED = 300.0
 	else:
-		SPEED = 200.0
-		
-	print(SPEED)
+		SPEED = 150.0
+	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
