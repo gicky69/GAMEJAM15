16f20145588a2dea14729c47c48ac1cedd860369
Carl Andrew Villanueva <carlandrewv@yahoo.com>
2024-07-24 13:59:14 +0800

    tested light toggle

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var SPEED = 200.0
 const JUMP_VELOCITY = -900.0
 var jump_damping = 35.0 # The rate at which the jump velocity is reduced
+var light = true
 
 @onready var sprite_2d = $Sprite2D
 
@@ -10,15 +11,22 @@ var jump_damping = 35.0 # The rate at which the jump velocity is reduced
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	
 	if (velocity.x > 1 or velocity.y < -1):
 		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "default"
+		
+	if Input.is_action_just_pressed("light"):
+		if light:
+			sprite_2d.animation = "dark-idle"
+			light = false
+		else:
+			sprite_2d.animation = "default"
+			light = true
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
