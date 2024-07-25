91ef002a028b8fdd864a6819f055880f9a2a2aab
Gerard Patrick <gmbriones@studnet.apc.edu.ph>
2024-07-25 20:03:56 +0800

    menu

---
+++ Main_menue.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_settings_button_up():
+	pass # Replace with function body.
+
+
+func _on_about_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
